fee14309696663fc71d4827b1977449984233246
BCombs <bill@Bills-MacBook-Pro.local>
2019-03-24 14:51:39 -0400

    Refactored contacts state

---
+++ src/components/Contacts.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import Contact from './Contact';
+
+class Contacts extends Component {
+    state = {
+        contacts: [
+            {
+                id: 1,
+                name: "John Doe",
+                email: "jdoe@mail.com",
+                phone: "555-555-5555"
+            },
+            {
+                id: 2,
+                name: "Jane Doe",
+                email: "jane@mail.com",
+                phone: "555-555-1234"
+            },
+            {
+                id: 3,
+                name: "Mark Smith",
+                email: "mark@mail.com",
+                phone: "555-555-4321"
+            }
+        ]
+    }
+  render() {
+      const { contacts } = this.state;
+    return (
+      <div>
+        {contacts.map(contact => (
+            <Contact key={contact.id} contact={contact} />
+        ))}
+      </div>
+    )
+  }
+}
+
+export default Contacts;
